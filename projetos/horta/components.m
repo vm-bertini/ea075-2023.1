850f0a4032e94beff0856d19ce15b4ee27fed82c
marifmenezes <128512011+marifmenezes@users.noreply.github.com>
2023-06-15 10:53:24 -0300

    Update components.m

--- projetos/horta/components.m
+++ projetos/horta/components.m
@@ -2,10 +2,10 @@ A seguir serão listados todos os componentes utilizados para o desenvolvimento 
 1. ATtiny40 (https://www.microchip.com/en-us/product/ATTINY40)
 2. Sensor de Umidade de Solo para Arduino 			
 3. Vávula solenóide água 110V  			
-4. Módulo Relé 5V 10A 1 Canal com Optoacoplador	https://www.usinainfo.com.br/rele-arduino/modulo-rele-5v-10a-1-canal-com-optoacoplador-para-esp32-e-arduino-2533.html
+4. Módulo Relé 5V 10A 1 Canal com Optoacoplador	
 5. Fonte de Alimentação para Arduino 9VDC 1A 			
-6. Botão 	
+6. Botão (https://www.hdk.co.jp/pdf/eng/e291702.pdf)
 7. Interruptor 	 			
-8. LED 				
+8. LED vermelho				
 9. Lampada 
 10. Sensor de luminosidade 
